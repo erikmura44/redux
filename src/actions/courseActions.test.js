b0c44e14dce55e46be77cccfb8479d4ceb48bcd3
Mura, Erik <erik.mura@vertafore.com>
2017-11-14 15:40:28 -0700

    tested actions

---
+++ src/actions/courseActions.test.js
@@ -0,0 +1,24 @@
+import expect from 'expect';
+import * as courseActions from './courseActions';
+import * as types from './actionTypes';
+
+//Test async action
+describe('Course Actions', () => {
+    describe('createCourseActions', () => {
+        it('should create a CREATE_COURSE_SUCCESS action', () => {
+            //arrange
+            const course = {id: 'clean-code', title: 'Clean Code'};
+            const expectedAction = {
+                type: types.CREATE_COURSE_SUCCESS,
+                course: course
+            };
+
+            //actionTypes.js
+            const action = courseActions.createCourseSuccess(course);
+
+            //assert
+            expect(action).toEqual(expectedAction);
+    
+        });
+    });
+});
